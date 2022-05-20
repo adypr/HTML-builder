5f3322e8b6e194769a41bda828b1acf6e4979c28
Adypr <workady2015@gmail.com>
2022-05-20 16:07:39 +0300

    feat: add task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fsp = require('fs/promises');
+const path = require('path');
+const pathName = path.join(__dirname, 'secret-folder');
+
+console.log('Note: File sizes are calculated in binary');
+fsp.readdir(pathName).then(fileNames => {
+                        fileNames.map(async (file) => {
+                            const fileName = path.join(pathName, file);
+                            const isDirectory = await fsp.stat(fileName).then(path => path.isDirectory());
+                            const fileInfo = path.parse(fileName);
+                            let fileSize;
+                            await fsp.stat(fileName).then(file => fileSize = file.size / 1024);
+                            if (!isDirectory) console.log(`${fileInfo.name} - ${fileInfo.ext.slice(1)} - ${fileSize}kb`);
+                        })
+                    })
+                    
