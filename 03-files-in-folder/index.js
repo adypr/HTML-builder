e8696f74d03124827eb446cafab8e0cefcad2e17
Adypr <workady2015@gmail.com>
2022-05-21 17:19:48 +0300

    feat: add task 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,13 +4,13 @@ const pathName = path.join(__dirname, 'secret-folder');
 
 console.log('Note: File sizes are calculated in binary');
 fsp.readdir(pathName).then(fileNames => {
-                        fileNames.map(async (file) => {
-                            const fileName = path.join(pathName, file);
-                            const isDirectory = await fsp.stat(fileName).then(path => path.isDirectory());
-                            const fileInfo = path.parse(fileName);
-                            let fileSize;
-                            await fsp.stat(fileName).then(file => fileSize = file.size / 1024);
-                            if (!isDirectory) console.log(`${fileInfo.name} - ${fileInfo.ext.slice(1)} - ${fileSize}kb`);
-                        })
-                    })
+  fileNames.map(async (file) => {
+    const fileName = path.join(pathName, file);
+    const isDirectory = await fsp.stat(fileName).then(path => path.isDirectory());
+    const fileInfo = path.parse(fileName);
+    let fileSize;
+    await fsp.stat(fileName).then(file => fileSize = file.size / 1024);
+    if (!isDirectory) console.log(`${fileInfo.name} - ${fileInfo.ext.slice(1)} - ${fileSize}kb`);
+  });
+});
                     
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fsp = require('fs/promises');
+const path = require('path');
 
+const filesCopy = path.join(__dirname, 'files-copy');
+
+fsp.rm(filesCopy, {recursive: true})
+  .then(() => fsp.mkdir(filesCopy))
+  .catch(() => fsp.mkdir(filesCopy))
+  .then(() => copyFiles(path.join(__dirname, 'files'), filesCopy))
+  .catch(() => copyFiles(path.join(__dirname, 'files'), filesCopy));
+
+const copyFiles = async (folder) => {
+  const arr = await fsp.readdir(folder);
+  arr.map(async (file) => {
+    const filePath = path.join(folder, file);
+    await fsp.copyFile(filePath, path.join(filesCopy, file));
+  });
+};
