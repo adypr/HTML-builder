14a29f4a3b3d9b78a55b934ef92cbd17e67be31c
Adypr <workady2015@gmail.com>
2022-05-23 20:42:46 +0300

    refactor: check eslint

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -7,11 +7,11 @@ const filePath = path.join(__dirname, 'text.txt' );
 const stream = new fs.ReadStream(filePath);
 
 stream.on('readable', () => {
-    const data = stream.read();
-    if (data !== null) stdout.write(data);
-})
+  const data = stream.read();
+  if (data !== null) stdout.write(data);
+});
 
 stream.on('error', (err) => {
-    if (err) console.log('Ошибка выполнения программы:', err);
-})
+  if (err) console.log('Ошибка выполнения программы:', err);
+});
 
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,25 +7,25 @@ const partingWords = 'Have a good day!';
 
 const stream = fs.createReadStream(path.join(__dirname, 'newfile.txt' ), 'utf-8');
 stream.on('error', async () => {
-   await fs.writeFile(path.join(__dirname, 'newfile.txt' ), '', (err) => {
-        if (err) throw (err);
-    })
-})
+  await fs.writeFile(path.join(__dirname, 'newfile.txt' ), '', (err) => {
+    if (err) throw (err);
+  });
+});
 
 stdout.write('Hi!\nPlease, type some text\nYour text will be written to a file newfile.txt\n');
 stdin.on('data', (data) => {
-    if (data.toString().slice(0, 4) === 'exit') {
-        console.log(partingWords);
-        exit();
-    }
+  if (data.toString().slice(0, 4) === 'exit') {
+    console.log(partingWords);
+    exit();
+  }
      
-    fs.appendFile(path.join(__dirname, 'newfile.txt' ), data, (err) => {
-        if (err) throw err;
-        console.log('You can continue typing. For finish type \'exit\' or Ctrl + C');
-    })
-})
+  fs.appendFile(path.join(__dirname, 'newfile.txt' ), data, (err) => {
+    if (err) throw err;
+    console.log('You can continue typing. For finish type \'exit\' or Ctrl + C');
+  });
+});
 
 process.on('SIGINT', () => {
-    console.log(partingWords)
-    exit();
-})
+  console.log(partingWords);
+  exit();
+});
